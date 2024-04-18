67057ba1e7182d033f540e5e66eb78b2345ab25b
Jose Tucci Jr <tucci.jr@gmail.com>
2024-04-18 16:32:51 -0300

    :sparkle: Integrate PayPal sandbox checkout

---
+++ BlazorWasmApi.Web/BlazorWasmApi.Web/Components/Pages/Checkout.razor.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using BlazorWasmApi.Models.Dtos;
+using BlazorWasmApi.Web.Services.Contracts;
+
+namespace BlazorWasmApi.Web.Components.Pages;
+
+public partial class Checkout
+{
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; }
+
+    protected IEnumerable<CartItemDto> ShoppingCartItems { get; set; }
+
+    protected int TotalQty { get; set; }
+
+    protected decimal PaymentAmount { get; set; }
+
+    protected string PaymentDescription { get; set; }
+
+    [Inject]
+    public IShoppingCartService ShoppingCartService { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        try
+        {
+            ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+
+            if (ShoppingCartItems != null)
+            {
+                Guid orderGuid = Guid.NewGuid();
+
+                PaymentAmount = ShoppingCartItems.Sum(x => x.TotalPrice);
+                TotalQty = ShoppingCartItems.Sum(x => x.Qty);
+                PaymentDescription = $"O_{HardCoded.UserId}_{orderGuid}";
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        try
+        {
+            if (firstRender)
+            {
+                await JSRuntime.InvokeVoidAsync("initPayPalButton");
+            }
+        }
+        catch (Exception)
+        {
+            throw;
+        }
+    }
+}
